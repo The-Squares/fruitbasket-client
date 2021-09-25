990c4b6cccbcd8e9805fe70d88d042671351dadb
Cameron <43302238+biggant1@users.noreply.github.com>
2021-09-25 16:05:06 -0700

    start of offers page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "Screens/Home/Home";
+import Offers from "Screens/Offers/Offers";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/offers" exact component={Offers} />
       </Switch>
     </Router>
   );
---
+++ src/Screens/Offers/Offers.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Offers.css";
+import OfferButton from "Components/OfferButton";
+import BottomBar from "Components/BottomBar";
+
+function Offers() {
+  return (
+    <div className="Offers">
+      <div className="offerButtons">
+        {new Array(20).fill(
+          <OfferButton
+            fruit_name="Apples"
+            location="Beverly Hills - CA"
+            distance="5 minutes away"
+          />
+        )}
+      </div>
+      <BottomBar></BottomBar>
+    </div>
+  );
+}
+
+export default Offers;
