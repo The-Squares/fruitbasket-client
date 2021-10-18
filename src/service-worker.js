82da73f7f9bce9519b884421f6a653fec41750d3
Cameron <43302238+biggant1@users.noreply.github.com>
2021-10-17 19:14:54 -0700

    return to old service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -44,8 +44,7 @@ registerRoute(
 
     return true;
   },
-  // createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
-  new NetworkFirst()
+  createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
 );
 
 // An example runtime caching route for requests that aren't handled by the
