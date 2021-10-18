80cf9b0ec25593b2c4fd1b3648d195950ead1db4
Cameron <43302238+biggant1@users.noreply.github.com>
2021-10-17 19:33:36 -0700

    fix error handle

--- src/Util/signUp.js
+++ src/Util/signUp.js
@@ -1,47 +1,52 @@
 const signUp = async (e, imageRef, history, dimensions) => {
-  e.preventDefault();
-  let answers = Array.from(e.target)
-    .slice(0, 4)
-    .map((target) => [target.dataset.key, target.value]);
+  try {
+    e.preventDefault();
+    let answers = Array.from(e.target)
+      .slice(0, 4)
+      .map((target) => [target.dataset.key, target.value]);
 
-  let userData = {};
-  answers.forEach(([key, value]) => {
-    userData[key] = value;
-  });
+    let userData = {};
+    answers.forEach(([key, value]) => {
+      userData[key] = value;
+    });
 
-  let checkbox = e.target.querySelector(".volunterInput input");
-  userData["volunteer"] = checkbox.checked;
+    let checkbox = e.target.querySelector(".volunterInput input");
+    userData["volunteer"] = checkbox.checked;
 
-  let response = await fetch(
-    `${process.env.REACT_APP_API_BASE_URL}/api/users`,
-    {
-      method: "POST",
-      body: JSON.stringify(userData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-  let user = await response.json();
+    let response = await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/api/users`,
+      {
+        method: "POST",
+        body: JSON.stringify(userData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    let user = await response.json();
 
-  const formData = new FormData();
-  formData.append("image", imageRef.current.files[0]);
+    const formData = new FormData();
+    formData.append("image", imageRef.current.files[0]);
 
-  dimensions.top = dimensions.y;
-  dimensions.left = dimensions.x;
-  delete dimensions.x;
-  delete dimensions.y;
-  formData.append("dimensions", JSON.stringify(dimensions));
+    dimensions.top = dimensions.y;
+    dimensions.left = dimensions.x;
+    delete dimensions.x;
+    delete dimensions.y;
+    formData.append("dimensions", JSON.stringify(dimensions));
 
-  await fetch(
-    `${process.env.REACT_APP_API_BASE_URL}/api/users/${user["_id"]}/image`,
-    {
-      method: "POST",
-      body: formData,
-    }
-  );
+    await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/api/users/${user["_id"]}/image`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
 
-  localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("user", JSON.stringify(user));
+  } catch (e) {
+    console.log(e);
+  }
+
   history.push("/");
 };
 
--- src/Util/submitForm.js
+++ src/Util/submitForm.js
@@ -1,41 +1,45 @@
 const submitForm = async (e, history, imageRef) => {
-  e.preventDefault();
-  let answers = Array.from(e.target)
-    .filter((target) => target.dataset.key)
-    .map((target) => [target.dataset.key, target]);
+  try {
+    e.preventDefault();
+    let answers = Array.from(e.target)
+      .filter((target) => target.dataset.key)
+      .map((target) => [target.dataset.key, target]);
 
-  let data = {};
-  answers.forEach(([key, value]) => {
-    if (key === "organic") return (data[key] = value.checked);
-    data[key] = value.value;
-  });
+    let data = {};
+    answers.forEach(([key, value]) => {
+      if (key === "organic") return (data[key] = value.checked);
+      data[key] = value.value;
+    });
 
-  data.timestamp = Date.now();
-  data.user = JSON.parse(localStorage.getItem("user"))?._id;
+    data.timestamp = Date.now();
+    data.user = JSON.parse(localStorage.getItem("user"))?._id;
 
-  let response = await fetch(
-    `${process.env.REACT_APP_API_BASE_URL}/api/offers`,
-    {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+    let response = await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/api/offers`,
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  let offer = await response.json();
+    let offer = await response.json();
 
-  const formData = new FormData();
-  formData.append("image", imageRef.current.files[0]);
+    const formData = new FormData();
+    formData.append("image", imageRef.current.files[0]);
 
-  await fetch(
-    `${process.env.REACT_APP_API_BASE_URL}/api/offers/${offer["_id"]}/image`,
-    {
-      method: "POST",
-      body: formData,
-    }
-  );
+    await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/api/offers/${offer["_id"]}/image`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
 
   history.push("/");
 };
