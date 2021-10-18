5e0e870962b16f4fdcb0d1b1a999ca8803567059
Cameron <43302238+biggant1@users.noreply.github.com>
2021-10-17 19:08:27 -0700

    service worker fix

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,7 @@ import { clientsClaim } from "workbox-core";
 import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
+import { StaleWhileRevalidate, NetworkFirst } from "workbox-strategies";
 
 clientsClaim();
 
@@ -44,7 +44,8 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
+  // createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
+  new NetworkFirst()
 );
 
 // An example runtime caching route for requests that aren't handled by the
